b630b19b3e34ceae3fea49f8d52427e9b9f6fd56
Bradley Kreider <user.email=1699478+bkreider@users.noreply.github.com>
2022-07-22 12:40:56 -0400

    Added windows build

---
+++ recipe/bld.bat
@@ -0,0 +1,32 @@
+:: cmd
+
+:: Isolate the build.
+mkdir build
+if errorlevel 1 exit 1
+
+cmake ^
+  -G"Ninja" ^
+  -S %SRC_DIR% ^
+  -B %SRC_DIR%\build ^
+  -DFMT_TEST=OFF ^
+  -DFMT_DOC=OFF ^
+  -DCMAKE_INSTALL_PREFIX="%PREFIX%"
+
+cd build
+if errorlevel 1 exit /b 1
+
+:: Build.
+echo "Building..."
+ninja
+if errorlevel 1 exit /b 1
+
+:: Install.
+echo "Installing..."
+ninja install
+if errorlevel 1 exit /b 1
+
+
+:: Error free exit.
+echo "Error free exit!"
+exit 0
+
